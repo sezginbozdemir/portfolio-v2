290ccf6bdb52ec03319b1cee2694613717a00368
sezginbozdemir <88796191+sezginbozdemir@users.noreply.github.com>
2025-04-24 17:15:22 +0300

    vite image optimizer tool

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpeg: { quality: 75 },
+      jpg: { quality: 75 },
+      webp: { lossless: false, quality: 75 },
+      avif: { lossless: false, quality: 60 },
+      includePublic: true,
+      logStats: true,
+    }),
+  ],
   preview: {
     allowedHosts: ["sezdev.com", "localhost", "127.0.0.1"],
   },
